bd8e94b9e6dbeb32c58c2ed1f8446143d17f190f
Sohel Ahmed <37261659+Sohel-Ahmed-Rana@users.noreply.github.com>
2020-02-24 17:42:49 +0600

    Updates

---
+++ precache-manifest.adc37591cea2ab10f2617b70da27d762.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75869eb47592fe423b3c660c3a7142ad",
+    "url": "/sohel/index.html"
+  },
+  {
+    "revision": "06add6ec37c488eb5dea",
+    "url": "/sohel/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c2eaaf956b65e7d8c5f7",
+    "url": "/sohel/static/js/2.c86eb69f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/sohel/static/js/2.c86eb69f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06add6ec37c488eb5dea",
+    "url": "/sohel/static/js/main.fe017476.chunk.js"
+  },
+  {
+    "revision": "fcdad9adaabfacb852cc",
+    "url": "/sohel/static/js/runtime-main.4da1bdf0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sohel/static/media/logo.5d5d9eef.svg"
+  }
+]);
