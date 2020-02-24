2912453674817bb385da037ff4bb164237325c63
Sohel Ahmed <37261659+Sohel-Ahmed-Rana@users.noreply.github.com>
2020-02-24 20:53:40 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './Header';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+    <Header/>
       <header className="App-header">
        <div>Sohel Ahmed</div>
       </header>
